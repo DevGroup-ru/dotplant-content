3549929ca78806c7899ac8312f078afe42cbaa74
fps01 <fps.06@mail.ru>
2016-09-16 10:54:00 +0300

    migration fix

--- src/migrations/m160912_115202_dotplant_core_add_demo_page.php
+++ src/migrations/m160912_115202_dotplant_core_add_demo_page.php
@@ -1,17 +1,81 @@
 <?php
 
+use DevGroup\Multilingual\models\Context;
 use DotPlant\Content\models\Page;
+use DotPlant\Content\models\PageExtended;
 use DotPlant\EntityStructure\models\BaseStructure;
 use DotPlant\EntityStructure\models\Entity;
-use DevGroup\Multilingual\models\Context;
+use DotPlant\Monster\DataEntity\StaticContentProvider;
 use yii\db\Migration;
-use DotPlant\Content\models\PageExtended;
 use yii\db\Query;
 
 class m160912_115202_dotplant_core_add_demo_page extends Migration
 {
     public function up()
     {
+        $this->insert(
+            '{{%template}}',
+            [
+                'name' => 'Example page layout',
+                'key' => 'example',
+                'is_layout' => false,
+                'packed_json_providers' => json_encode([
+                    [
+                        'class' => StaticContentProvider::class,
+                        'entities' => [
+                            'best' => [
+                                't001' => [
+                                    'title' => 'BEST company EVER',
+                                    'blocks' => [
+                                        [
+                                            'name' => [
+                                                'href' => '#block1',
+                                                'anchor' => 'Block #1',
+                                            ],
+                                            'content' => 'Lorem ipsum UNO block',
+                                        ],
+                                        [
+                                            'name' => [
+                                                'href' => '#block2',
+                                                'anchor' => 'Second block',
+                                            ],
+                                            'content' => 'Lorem ipsum DOS blockos',
+                                        ],
+                                    ],
+                                ],
+                            ],
+                        ],
+                    ],
+                ]),
+            ]
+        );
+        $templateId = $this->db->lastInsertID;
+        $this->insert(
+            '{{%template_region}}',
+            [
+                'template_id' => $templateId,
+                'sort_order' => 2,
+                'name' => 'Common block',
+                'key' => 'best',
+                'entity_dependent' => 0,
+                'packed_json_content' => json_encode([
+                    't001' => [
+                        'material' => 'core.frontend-monster.content-blocks.content-block-001',
+                    ],
+                ]),
+            ]
+        );
+        $this->insert(
+            '{{%template_region}}',
+            [
+                'template_id' => $templateId,
+                'sort_order' => 1,
+                'name' => 'Page content region',
+                'key' => 'content',
+                'entity_dependent' => 1,
+                'packed_json_content' => '[]',
+            ]
+        );
         $entityId = Entity::getEntityIdForClass(Page::class);
         $contexts = Context::find()->all();
         foreach ($contexts as $context) {
@@ -40,7 +104,6 @@ class m160912_115202_dotplant_core_add_demo_page extends Migration
                         'language_id' => $language->id,
                         'packed_json_content' => '{"content":{"p01":{"material":"core.frontend-monster.content-blocks.content-block-001"},"p02":{"material":"core.frontend-monster.content-blocks.content-block-002"}}}',
                         'packed_json_providers' => '[]',
-//                        'layout_id' => '1',
                     ]
                 );
             }
